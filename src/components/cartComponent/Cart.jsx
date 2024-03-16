7809a85aa84240e35e0c6943516dfa9e6290b56c
AatishKamble <143265782+AatishKamble@users.noreply.github.com>
2024-03-16 11:36:36 +0530

    initial commit

---
+++ src/components/cartComponent/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Cart.css"
+const Cart = () => {
+  return (
+  <>
+  <div className='cart'>
+    <img src="../../src/assets/laptop.webp" alt="Laptop" />
+
+  </div>
+  
+  </>
+  )
+}
+
+export default Cart
